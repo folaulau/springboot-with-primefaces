31c65a5e529bd77004f58179cf4fa035508380bc
Folau Kaveinga <folaukaveinga@gmail.com>
2024-11-20 13:46:04 -0700

    setup

---
+++ src/main/java/com/folau/primefaces/SpringbootWithPrimefacesApplication.java
@@ -0,0 +1,13 @@
+package com.folau.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithPrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithPrimefacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folau/primefaces/SpringbootWithPrimefacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folau.primefaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithPrimefacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
